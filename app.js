aef026869d956ddaa707b457edfe008df94c06ca
devdariddhii <161798819+devdariddhii@users.noreply.github.com>
2024-07-02 20:12:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+//var app = express();
+
+var mongoose = require('mongoose');
+
+// MongoDB Connection
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+.then(()=>{
+ console.log('Connected to MongoDB');
+})
+.catch(err => {
+ console.log("Error in DB Connection " +err);
+});
+
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Middlewares
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', indexRouter);
+// Define routes here
+
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // Render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
